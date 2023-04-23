f051f235a9c804e80d1c2e8d63cd8b1bed9ec0c5
diaa <a2diaa@uwaterloo.ca>
2023-04-23 22:54:23 +0000

    setting experiment parameters

--- scripts/mpc_inference_metalauncher.py
+++ scripts/mpc_inference_metalauncher.py
@@ -1,12 +1,10 @@
 import os
 
-# delays = ["0.0", "0.05", "0.1", "0.15", "0.2"]
-delays = ["0.0", "0.01"]
-models = ["resnet18", "resnet32", "resnet50", "resnet110", "vgg16", "vgg16_bn", "minionn"]
-# datasets = ["cifar10", "cifar100", "imagenet"]
+delays = ["0.0", "0.01", "0.05", "0.1"]
+models = ["resnet18", "resnet32", "resnet50", "resnet110", "vgg16_bn", "minionn"]
 datasets = ["cifar10", "cifar100"]
 config_folder = "configs"
-configs = ["default16.yaml", "crypten10.yaml", "florian10.yaml", "honeybadger10.yaml", "crypten8.yaml"]
+configs = ["default16.yaml", "crypten10.yaml", "florian10.yaml", "honeybadger10.yaml"]
 device_commands = ["", "--use-cuda"]
 base_command = "python3 examples/mpc_inference/launcher.py --multiprocess --world_size 2 --n-batches 3 --skip-plaintext"
 base_command += " --delays " + " ".join(delays)
