4abfdb37fba3392a9801ae110f7c83ea8f53206b
diaa <a2diaa@uwaterloo.ca>
2023-06-05 18:45:05 -0400

    experiment wrappers

--- scripts/timing_experiments.py
+++ scripts/timing_experiments.py
@@ -16,7 +16,7 @@ models = {
 
 configs = ["espn12.yaml", "default12.yaml", "crypten12.yaml", "honeybadger12.yaml"]
 
-device_commands = ["--use-cuda"]
+device_commands = ["--use-cuda", ""]
 
 base_command = f"python3 examples/mpc_inference/launcher.py --multiprocess --world_size 2 " \
                f" --skip-plaintext " \
@@ -28,21 +28,9 @@ base_command += " --delays " + " ".join(delays)
 for dataset in datasets:
     for model in models[dataset]:
         for run in [1]:
-            # cmd = None
             cmd = base_command + f" --dataset {dataset}" \
                                  f" --model-type {model} "
-            # for model_folder in os.listdir(f"{models_folder}/{dataset}"):
-            #    if model_folder.startswith(f"{model}"):
-            #        model_file = f"{models_folder}/{dataset}/{model_folder}/run_{run}/best_model.pth"
-            #        print(model_file)
-            #        cmd = base_command + f" --dataset {dataset}" \
-            #                             f" --model-type {model} " \
-            #                             f"--resume " \
-            #                             f"--model-location {model_file}"
-            #        break
-
-            if cmd is not None:
-                for device_cmd in device_commands:
-                    for config in configs:
-                        cmd += f" --config {config_folder}/{config} {device_cmd}"
-                        os.system(cmd)
+            for device_cmd in device_commands:
+                for config in configs:
+                    cmd += f" --config {config_folder}/{config} {device_cmd}"
+                    os.system(cmd)
