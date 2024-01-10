ec17d9baa54b76ab66b18356bc850f854682275e
Diaa <a2diaa@uwaterloo.ca>
2024-01-10 16:16:57 -0500

    aws script

--- aws.sh
+++ aws.sh
@@ -2,6 +2,6 @@
 
 python3 scripts/aws_launcher.py \
 --ssh_key_file=/home/a2diaa/.ssh/mac,/home/a2diaa/.ssh/mac \
---instances=i-09dfba413e877b802,i-0895a49d766f9ef6c \
---regions=us-west-1,us-east-2 \
+--instances=i-05c9490a17d3acb9e,i-0f5e5547b00fdced1 \
+--regions=eu-central-1,us-east-2 \
 scripts/timing_experiments.py
